ab5f5433fc1bcd5ec20dd4c8df8bc2ba0202d8e5
Celestino Caterino <celestecaterino@gmail.com>
2024-10-18 08:00:29 +0200

    Created attendee request validators

---
+++ app/Http/Requests/UpdateAttendeeRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateAttendeeRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors()], 422));
+    }
+}
